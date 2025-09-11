5c7f981a902570d81923bfd84980be55f4ee0dbe
kacper <kacper@iqp.pl>
2025-09-11 12:23:43 +0200

    created

---
+++ src/components/models/SpotifyData.ts
@@ -0,0 +1,21 @@
+// src/types/spotify.ts
+export interface SpotifyImage {
+    url: string;
+    height?: number;
+    width?: number;
+}
+
+export interface SpotifyArtist {
+    id: string;
+    name: string;
+    genres: string[];
+    popularity: number;
+    images: SpotifyImage[];
+}
+
+export interface SpotifyTopArtistsResponse {
+    items: SpotifyArtist[];
+    limit: number;
+    offset: number;
+    total: number;
+}
